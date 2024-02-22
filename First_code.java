eb2271e9bc830f1340f58c21098c354b00974baf
sujani-R <157608467+sujani-R@users.noreply.github.com>
2024-02-22 13:41:26 +0530

    Add files via upload

---
+++ First_code.java
@@ -0,0 +1,54 @@
+package myjavaproject;
+
+public class First_code 
+{
+	public static void main(String args[])
+	{
+		int a=14;// primitive data type
+		int b=20;
+		int c=0;
+//		c=a;
+//		a=b;
+//		b=c;
+//		System.out.println(a);
+//		System.out.println(b);
+		//double d=2.22;
+		//char c='n';
+		//boolean b= true;
+		//String s ="Java"; //derived data type
+		//System.out.println(c);
+		int i=0;
+		for (i=1;i<=10;i++) //mul table of 5
+		{
+		System.out.println(i*5);
+		}
+		
+		
+//		int day =4;
+//		switch (day)
+//		{
+//		case 1: System.out.println("Sunday");break;
+//		case 2: System.out.println("Monday");break;
+//		default: System.out.println("weekday");
+//		}
+//		if (a==11)
+//		{
+//			System.out.println("eleven");
+//		}
+//		else if (a==12)
+//		{
+//			System.out.println("twelve");
+//		}
+//		else if (a==13)
+//		{
+//			System.out.println("thirteen");
+//		}
+//		else 
+//		{
+//			System.out.println("fourteen");
+//		}
+//		}
+	//System.out.println("This is my first java code");
+	
+	}
+}
